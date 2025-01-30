12b294f48da943525183aaa1dd0afc0996af3135
RESEGO MOTLHASI <remotlhjhb024@student.wethinkcode.co.za>
2025-01-30 10:37:58 +0200

    Wrote code that removes white space in the beginning and end of a string

---
+++ remove_whitespace.py
@@ -0,0 +1,6 @@
+#prompts user for the string or message
+user_string = input("Enter a string: ")
+
+#remove the trailing and leading whitespace
+removing_whitespace = user_string.strip()
+print(removing_whitespace)
